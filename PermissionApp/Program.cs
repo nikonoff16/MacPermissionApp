7db4aa170ad6318f2e5f6fbd4713e36e3237570b
Viktor Osipov <v.osipov08@hotmail.com>
2023-08-02 23:21:27 +0500

    Added new way to interact with the app.

--- PermissionApp/Program.cs
+++ PermissionApp/Program.cs
@@ -4,23 +4,71 @@ namespace PermissionApp;
 
 class Program
 {
-    static async Task Main(string[] args)
+    static async Task<int> Main(string[] args)
     {
         var appPathArgument = new Argument<string>
-            ("path", "Path to the program that needs to add permissions");
+            (name: "path", description: "Path to the program that needs to add permissions");
+
+        var addSinglePermissionArgument = new Argument<string>
+            (name: "permission", description: "Single permission to add", getDefaultValue: () => "NotAnPermission");
+        
+        var addPermissionsOption = new Option<string[]>(
+                name: "--add-multiple",
+                description: "Strings to search for when deleting entries.")
+            { IsRequired = false, AllowMultipleArgumentsPerToken = true};
+        addPermissionsOption.AddAlias("-m");
+
+        var defaultCommand = new Command("default", "Set permission to use a microphone and a camera on the device.")
+            {
+                appPathArgument
+            };
+
+        var addCommand = new Command("add", "Set permission or permissions");
+        addCommand.AddArgument(appPathArgument);
+        addCommand.AddArgument(addSinglePermissionArgument);
+        addCommand.AddOption(addPermissionsOption);
+
 
         var rootCommand = new RootCommand();
-        rootCommand.Add(appPathArgument);
+        rootCommand.AddCommand(defaultCommand);
+        rootCommand.AddCommand(addCommand);
 
-        rootCommand.SetHandler((appPathArgumentValue) =>
+        defaultCommand.SetHandler((appPathArgumentValue) =>
+        {
+            Console.WriteLine(Services.SetPermissions(appPathArgumentValue, UtilCommands.Camera));
+            Console.WriteLine(Services.SetPermissions(appPathArgumentValue, UtilCommands.Microphone));
+            Console.WriteLine(Services.SetPermissions(appPathArgumentValue, UtilCommands.ScreenCapture));
+        }, appPathArgument);
+        
+        addCommand.SetHandler((appPathArgumentValue, singlePermission, permissionsArray) =>
+        {
+            if (permissionsArray.Length == 0)
             {
-                Console.WriteLine($"<message> argument = {appPathArgumentValue}");
-                var appId = Services.GetAppId(appPathArgumentValue);
-                Console.WriteLine(appId);
-                Console.WriteLine(Services.GetPermissions(appId));
-            },
-            appPathArgument);
+                SetPermissionByString(appPathArgumentValue, singlePermission);
+            }
+            else
+            {
+                foreach (var permission in permissionsArray)
+                {
+                    SetPermissionByString(appPathArgumentValue, permission);
+                }    
+            }
 
-        await rootCommand.InvokeAsync(args);
+            
+        }, appPathArgument, addSinglePermissionArgument, addPermissionsOption);
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    private static void SetPermissionByString(string appPathArgumentValue, string permission)
+    {
+        if (Enum.IsDefined(typeof(UtilCommands), permission))
+        {
+            var realPermission = (UtilCommands)Enum.Parse(typeof(UtilCommands), permission);
+            Console.WriteLine(Services.SetPermissions(appPathArgumentValue, realPermission));
+        }
+        else
+        {
+            Console.WriteLine($"Permission {permission} does not exists.");
+        }
     }
 }
--- PermissionApp/Services.cs
+++ PermissionApp/Services.cs
@@ -5,24 +5,21 @@ namespace PermissionApp;
 
 class Services
 {
-    public static string GetPermissions(string appId, bool micro=true, bool camera=true, bool screenRecord=false) 
+    public static string SetPermissions(string appPath, Enum permissionName)  
     {
+        var appId = Services.GetAppId(appPath);
+        return GetPermissions(appId, permissionName.ToString());
+    }
+    private static string GetPermissions(string appId, string permissionName) 
+    {
+       
         if (File.Exists("tccplus"))
         {
-            string result = "";
-            if (micro) 
-            {
-                result += StartProcess("./tccplus", $"add {UtilCommands.Microphone} {appId}");
-            }
-            if (camera) 
-            {
-                result += StartProcess("./tccplus", $"add {UtilCommands.Camera} {appId}");
-            }
-            if (screenRecord) 
-            {
-                result += StartProcess("./tccplus", $"add {UtilCommands.ScreenCapture} {appId}");
-            }
-            return result;
+            return StartProcess("./tccplus", $"add {permissionName} {appId}");;
+        }
+        else if (String.IsNullOrEmpty(permissionName))
+        {
+            return "Received an empty permission name. Nothing to add I guess.";
         }
         else 
         {
@@ -30,16 +27,16 @@ class Services
         }
     }
 
-    static string FindId(string output)
+    private static string FindId(string output)
     {
-            string pattern = "identifier ['\"]\\w+.*?.\\w+['\"]";
+            const string pattern = "identifier ['\"]\\w+.*?.\\w+['\"]";
             var result = "";
 
-            RegexOptions options = RegexOptions.Multiline;
+            const RegexOptions options = RegexOptions.Multiline;
             
-            foreach (Match m in Regex.Matches(output, pattern, options).Cast<Match>())
+            foreach (var m in Regex.Matches(output, pattern, options).Cast<Match>())
             {
-                Console.WriteLine($"'{m.Value}' found at index {m.Index}.");
+                Console.WriteLine($"Internal app identifier '{m.Value}' has been found.");
                 result = m.Value;
                 result = result.Replace("identifier ", "");
                 result = result.Replace("\"", "");
@@ -47,7 +44,7 @@ class Services
             return result;
     }
 
-    public static string GetAppId(string appUrl) 
+    private static string GetAppId(string appUrl) 
     {
         var arguments = "-dr - " + "\"" + appUrl + "\"";
         var rawAppInfo = StartProcess("codesign", arguments);
@@ -55,17 +52,21 @@ class Services
         return test;
     }
 
-    public static string StartProcess(string appName, string arguments) 
+    private static string StartProcess(string appName, string arguments) 
     {
-        var psi = new ProcessStartInfo();
-        psi.FileName = appName;
-        psi.Arguments = arguments;
-        psi.UseShellExecute = false;
-        psi.RedirectStandardOutput = true;
+        var psi = new ProcessStartInfo
+        {
+            FileName = appName,
+            Arguments = arguments,
+            UseShellExecute = false,
+            RedirectStandardOutput = true
+        };
 
         using var process = Process.Start(psi);
+        if (process == null) return "Some errors has been raised while execution";
         using StreamReader reader = process.StandardOutput;
 
         return reader.ReadToEnd();
+
     }
 }
